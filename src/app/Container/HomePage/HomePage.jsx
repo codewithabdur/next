35571432c6d8a1b5d49ca2624273140f2801f70b
CodeWithAbdur <codewithabdur.react@gmail.com>
2024-03-01 14:47:50 +0530

    Next App

---
+++ src/app/Container/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+"use client";
+import { useEffect } from "react";
+
+const HomePage = () => {
+  useEffect(() => {
+    console.clear();
+  }, []);
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import HomePage from './Container/HomePage/HomePage'
+const page = () => {
+  return (
+    <>
+    <HomePage />
+    </>
+  )
+}
+
+export default page
